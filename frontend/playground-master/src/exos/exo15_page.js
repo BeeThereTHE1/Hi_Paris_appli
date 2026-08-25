0965b16cae8c23fc7747c2f8d9e040d854165d01
Benjamin WEIC <benjamin.weic@ensae.fr>
2026-08-25 14:11:10 +0200

    Increase timer duration from 2 to 7 seconds

--- frontend/playground-master/src/exos/exo15_page.js
+++ frontend/playground-master/src/exos/exo15_page.js
@@ -299,7 +299,7 @@ function startTutorial() {
     popup.appendChild(nextBtn);
     overlay.appendChild(popup);
     document.body.appendChild(overlay);
-    var timeLeft = 2;
+    var timeLeft = 7;
     function updateTimer() {
         if (timeLeft > 0) {
             timerSpan.innerText = "Temps de lecture restant : " + timeLeft + "s";
