b913a79f8a6fa6072f4c011b23a41ae8b5809a41
Benjamin WEIC <benjamin.weic@ensae.fr>
2026-08-25 13:54:54 +0200

    fix: add exo13.js with corrected exercise number (from ericpapain/Hi_Paris_Parcours_pedagogique)

---
+++ frontend/playground-master/src/exos/exo13.js
@@ -0,0 +1,59 @@
+"use strict";
+exports.__esModule = true;
+var state_1 = require("../state");
+var d3 = require("d3");
+exports.exo13 = {
+    hiddenControls: [
+        "regularization",
+        "regularizationRate",
+        "problem",
+        "percTrainData",
+        "batchSize",
+        "showTestData",
+        "xSquared",
+        "ySquared",
+        "xTimesY",
+        "cosX",
+        "cosY",
+        "sinX",
+        "sinY",
+        "weightEditor",
+        "biasEditor",
+        "seedControl",
+        "doubleRunButton"
+    ],
+    forcedState: {
+        dataset: state_1.datasets["spiral"],
+        activation: state_1.activations["relu"],
+        numHiddenLayers: 6,
+        networkShape: [8, 8, 8, 8, 8, 8],
+        x: true,
+        y: true,
+        xSquared: false,
+        ySquared: false,
+        xTimesY: false,
+        cosX: false,
+        sinX: false,
+        cosY: false,
+        sinY: false,
+        learningRate: 0.1,
+        regularization: null,
+        percTrainData: 50,
+        noise: 5,
+        batchSize: 10
+    }
+};
+function initExo13Popups() {
+    var overlay = d3.select("#exo13-popup-overlay");
+    var page1 = d3.select("#exo13-page1");
+    var page2 = d3.select("#exo13-page2");
+    overlay.style("display", "flex");
+    d3.select("#exo13-next-btn1").on("click", function () {
+        page1.style("display", "none");
+        page2.style("display", "block");
+    });
+    d3.select("#exo13-next-btn2").on("click", function () {
+        overlay.style("display", "none");
+    });
+}
+exports.initExo13Popups = initExo13Popups;
