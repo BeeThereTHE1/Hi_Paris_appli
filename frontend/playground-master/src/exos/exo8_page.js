284ec6b980ff240f56f9cf2da046e2a0bb0ef62a
Benjamin WEIC <benjamin.weic@ensae.fr>
2026-08-25 14:48:26 +0200

    Update title for Exercise #8 in tutorial overlay

--- frontend/playground-master/src/exos/exo8_page.js
+++ frontend/playground-master/src/exos/exo8_page.js
@@ -273,7 +273,7 @@ function startTutorial() {
     var popup = document.createElement('div');
     popup.className = 'tutorial-popup';
     var h3 = document.createElement('h3');
-    h3.innerText = translations && translations.title ? translations.title : "Exercice #7 Instability";
+    h3.innerText = translations && translations.title ? translations.title : "Exercise #8 : Instability";
     var p = document.createElement('p');
     var text = translations && translations.instructions && translations.instructions.general
         ? translations.instructions.general
