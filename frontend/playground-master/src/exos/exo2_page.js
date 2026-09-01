abc67a7115651ed35bf489246caf6f26180ce8b9
Benjamin WEIC <benjamin.weic@ensae.fr>
2026-09-01 18:39:15 +0200

    Refactor event listeners and cleanup methods

--- frontend/playground-master/src/exos/exo2_page.js
+++ frontend/playground-master/src/exos/exo2_page.js
@@ -21,6 +21,9 @@
 
       this._successShown = false;
       this._activeCleanup = null;
+      this._boundMessageHandler = null;
+      this._boundReposition = null;
+      this._activeHintMeta = null;
 
       this.init();
     }
@@ -29,8 +32,7 @@
       await this.initProgressContext();
       this.wireStandardActionButtons();
 
-      // EXO_SUCCESS from playground
-      window.addEventListener('message', (event) => {
+      this._boundMessageHandler = (event) => {
         if (!event || !event.data) return;
         if (event.data.type === 'EXO_SUCCESS' && (event.data.exoId == 2 || event.data.exoId == '2')) {
           this.unlockQuizButton(this.doneBtnId, '✨ Exercise Successful !!');
@@ -39,14 +41,35 @@
             this.showExerciseSuccessCongrats();
           }
         }
-      });
+      };
+      window.addEventListener('message', this._boundMessageHandler);
+
+      this._boundReposition = () => this.repositionActiveHint();
+      window.addEventListener('scroll', this._boundReposition, { passive: true });
+      window.addEventListener('resize', this._boundReposition);
 
       this.onIframeLoad(() => {
         if (this.isCompletedFromQuery()) return;
         this.startTutorial();
       }, 1200);
 
-      window.addEventListener('beforeunload', () => this.cleanupActiveListeners());
+      window.addEventListener('beforeunload', () => this.cleanupAll());
+    }
+
+    cleanupAll() {
+      this.cleanupActiveListeners();
+      this.clearHints();
+
+      if (this._boundMessageHandler) {
+        window.removeEventListener('message', this._boundMessageHandler);
+        this._boundMessageHandler = null;
+      }
+
+      if (this._boundReposition) {
+        window.removeEventListener('scroll', this._boundReposition);
+        window.removeEventListener('resize', this._boundReposition);
+        this._boundReposition = null;
+      }
     }
 
     startTutorial() {
@@ -117,20 +140,15 @@
       if (!target) return;
 
       var rect = this.getIframeRectFor(target);
+      if (!rect) return;
 
       var box = document.createElement('div');
       box.className = 'tutorial-highlight-box';
-      box.style.left = (rect.left - 15 + window.scrollX) + 'px';
-      box.style.top = (rect.top - 15 + window.scrollY) + 'px';
-      box.style.width = (rect.width + 30) + 'px';
-      box.style.height = (rect.height + 30) + 'px';
       document.body.appendChild(box);
 
       var dot = document.createElement('div');
       dot.className = 'tutorial-indicator-dot';
       dot.innerText = label;
-      dot.style.left = (rect.left - 15 + window.scrollX) + 'px';
-      dot.style.top = (rect.top - 15 + window.scrollY) + 'px';
       document.body.appendChild(dot);
 
       var tooltip = document.createElement('div');
@@ -140,11 +158,35 @@
         : '<h4 style="margin:0 0 8px 0;">' + title + '</h4><p style="margin:0;">' + text + '</p><div class="tooltip-arrow"></div>';
       document.body.appendChild(tooltip);
 
-      var tRect = tooltip.getBoundingClientRect();
-      tooltip.style.left = (rect.left + rect.width / 2 - tRect.width / 2 + window.scrollX) + 'px';
-      tooltip.style.top = (rect.bottom + 10 + window.scrollY) + 'px';
-
       this._activeHint = { box: box, dot: dot, tooltip: tooltip };
+      this._activeHintMeta = { selector: selector };
+
+      this.positionHintFromRect(rect);
+    }
+
+    positionHintFromRect(rect) {
+      if (!this._activeHint || !rect) return;
+
+      this._activeHint.box.style.left = (rect.left - 15 + window.scrollX) + 'px';
+      this._activeHint.box.style.top = (rect.top - 15 + window.scrollY) + 'px';
+      this._activeHint.box.style.width = (rect.width + 30) + 'px';
+      this._activeHint.box.style.height = (rect.height + 30) + 'px';
+
+      this._activeHint.dot.style.left = (rect.left - 15 + window.scrollX) + 'px';
+      this._activeHint.dot.style.top = (rect.top - 15 + window.scrollY) + 'px';
+
+      var tRect = this._activeHint.tooltip.getBoundingClientRect();
+      this._activeHint.tooltip.style.left = (rect.left + rect.width / 2 - tRect.width / 2 + window.scrollX) + 'px';
+      this._activeHint.tooltip.style.top = (rect.bottom + 10 + window.scrollY) + 'px';
+    }
+
+    repositionActiveHint() {
+      if (!this._activeHint || !this._activeHintMeta || !this._activeHintMeta.selector) return;
+      var target = this.findInIframe(this._activeHintMeta.selector);
+      if (!target) return;
+      var rect = this.getIframeRectFor(target);
+      if (!rect) return;
+      this.positionHintFromRect(rect);
     }
 
     clearHints() {
@@ -153,6 +195,7 @@
       if (this._activeHint.dot) this._activeHint.dot.remove();
       if (this._activeHint.tooltip) this._activeHint.tooltip.remove();
       this._activeHint = null;
+      this._activeHintMeta = null;
     }
 
     armOneShotDocumentClick(handler) {
@@ -189,8 +232,11 @@
 
     getIframeRectFor(el) {
       var iframe = document.querySelector('.exo-frame') || document.getElementById(this.iframeId);
+      if (!iframe || !el) return null;
+
       var iframeRect = iframe.getBoundingClientRect();
       var r = el.getBoundingClientRect();
+
       return {
         left: iframeRect.left + r.left,
         top: iframeRect.top + r.top,
